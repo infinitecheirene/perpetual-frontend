dcae88a2a309c202b3ac52c8ca835fb58f2b2734
infinitecheirene <infinitech.eirene@gmail.com>
2026-01-21 16:51:47 +0800

    Updated files

--- app/dashboard/admin/about-us/page.tsx
+++ app/dashboard/admin/about-us/page.tsx
@@ -22,12 +22,14 @@ export default function AboutUsPage() {
 
                 <div className="max-w-7xl mx-auto p-6">
                     <Tabs defaultValue="community">
-                        <TabsList className="grid grid-cols-4 max-w-3xl">
-                            <TabsTrigger value="community">Community</TabsTrigger>
-                            <TabsTrigger value="goals">Goals</TabsTrigger>
-                            <TabsTrigger value="mission">Mission</TabsTrigger>
-                            <TabsTrigger value="objectives">Objectives</TabsTrigger>
-                        </TabsList>
+                        <div className="flex justify-center items-center mb-10">
+                            <TabsList className="grid grid-cols-2 lg:grid-cols-4 lg:max-w-3xl gap-x-4 w-full md:auto">
+                                <TabsTrigger value="community">Community</TabsTrigger>
+                                <TabsTrigger value="goals">Goals</TabsTrigger>
+                                <TabsTrigger value="mission">Mission</TabsTrigger>
+                                <TabsTrigger value="objectives">Objectives</TabsTrigger>
+                            </TabsList>
+                        </div>
 
                         <TabsContent value="community">
                             <AdminCommunityPage />
